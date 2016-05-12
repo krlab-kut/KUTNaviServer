39d485f428e846dbe45b9f7b0eebcc58d8439237
tokikaze0604 <tokikaze0604@gmail.com>
2016-05-12 09:25:43 +0000

    Androidクライアント用のコントローラ作成

---
+++ app/helpers/narukos_helper.rb
@@ -0,0 +1,2 @@
+module NarukosHelper
+end
---
+++ test/controllers/informations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InformationsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
