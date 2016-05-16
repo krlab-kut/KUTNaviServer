39c868b272c540d4bba24ef95d2c08e37efa1b03
rerejii <hayakawa.shinya.kochi@gmail.com>
2016-05-16 17:21:30 +0900

    dependent destroyの設定の変更と、placeとlabのforeign_keyの設定を行いました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :favorite, foreign_key: :user_id
-  has_many :question, foreign_key: :user_id
+  has_many :favorite, foreign_key: :user_id, dependent: :destroy
+  has_many :question, foreign_key: :user_id, dependent: :destroy
   belongs_to :place, foreign_key: :place_id
 end
