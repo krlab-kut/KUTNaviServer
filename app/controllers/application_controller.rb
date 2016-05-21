653051b19b389787af47ce1f8d8e32312f36ed39
ntomoya <ntomoya1010@gmail.com>
2016-05-21 18:40:19 +0000

    ファイル名をusesからusersへの変更 status 200 OK の正確な表示 位置情報登録と混雑情報取得の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  $test_info = [{place_id: 0, test_counter: 0},
+                {place_id: 1, test_counter: 0}]
   protect_from_forgery with: :exception
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def update
-    # user_id = check_uuid(:params[:uuid])
-    # @user = User.find(user_id)
+=begin
+    user_id = check_uuid(:params[:uuid])
+    @user = User.find(user_id)
+=end
     @user = User.find(params[:user_id])
+    # 前にいた場所の混雑情報カウンタの値を1引く
+    $test_info.each do |info|
+      info[:test_counter] -= 1 if @user.place_id == info[:place_id]
+    end
+    # 今いる場所の混雑情報カウンタの値を加算する
+    $test_info.each do |info|
+      info[:test_counter] += 1 if params[:place_id] == info[:place_id]
+    end
+    # user情報の更新
     @user.update(place_id: params[:place_id])
-    @hoge = { status: "200 OK" }
-    render :json => @hoge
+
+    @hoge = { status: "200 OK", foo: "bar"}
+    # render :json => @hoge
+    render 'users/update'
   end
 
   def show
-    # check_uuid()
+    render 'users/show'
+=begin
+    user_id = check_uuid(:params[:uuid])
+    @user = User.find(user_id)
+=end
+    @user = User.find(params[:user_id])
   end
 end
