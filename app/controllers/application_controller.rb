ef1c7a6d54fc7c2bd8bcb4cd631c08b864d7d2fd
tokikaze0604 <tokikaze0604@gmail.com>
2016-06-16 10:14:36 +0000

    ServerAPIKey更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require 'gcm'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  $gcm = GCM.new("AIzaSyDZmJt5EnLoREiyZ2Gu2y1OycO35eAXKwo")
+  $gcm = GCM.new("AIzaSyBln4YyKBgjB4ZYx82XQ3ijgpH0kuPsBWE")
   $congestion_info = [
                         {place_id: 1, counter: 0},
                         {place_id: 2, counter: 0}
