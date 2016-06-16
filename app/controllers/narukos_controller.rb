c80ec7c0e0cc64c80254bb654adc242c100b9fc3
tokikaze0604 <tokikaze0604@gmail.com>
2016-06-16 21:51:48 +0900

    narukoコントローラ修正

--- app/controllers/narukos_controller.rb
+++ app/controllers/narukos_controller.rb
@@ -1,9 +1,9 @@
 class NarukosController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def GCMpush
-    registration_ids = []
-    options = {data: {naruko: true}, collapse_key: "updated_score"}
+  def GCMpush(ids)
+    registration_ids = ids#User.find(4).registration_id#create_params[:user_id]
+    options = {data: {message: "naruko"}, collapse_key: "updated_score"}
     $gcm.send(registration_ids, options)
   end
 
@@ -13,7 +13,8 @@ class NarukosController < ApplicationController
     @user = User.find(user_id)
 =end
     # naruko情報の登録
-    GCMpush()
+    @user = User.where("place_id != ? and place_id = ?", create_params[:user_id], create_params[:place_id]).pluck(:id)
+    GCMpush(@user)
     @res = { status: "200 OK" }
   end
 
