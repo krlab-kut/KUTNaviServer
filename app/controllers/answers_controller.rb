4124d8e3f134de5b02b8fc2ed24d3c9001aee3b5
rerejii <hayakawa.shinya.kochi@gmail.com>
2016-06-19 21:15:23 +0900

    回答情報取得の改修

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,23 @@
 class AnswersController < ApplicationController
+
+  def index
+    #そもそもuser_idとlatest_atが無い、受け取れていない場合の判定
+    unless index_params.has_key?(:user_id)# && index_params.has_key?(:question_id) && index_params.has_key?(:latest_at)
+      @answer = {status: "400 Bad_Request"}
+      return
+    end
+    #受け取ったuser_idがデータベースに存在しない場合の判定
+    unless User.exists?(id: index_params[:user_id]) && Question.exists?(id: index_params[:question_id])
+      @answer = {status: "404 Not_found"}
+      return
+    end
+    @answer = Answer.where("question_id = ?", index_params[:question_id])
+    @time = {timestamp: index_params[:latest_at]}
+  end
+
+  private
+  def index_params
+      params.permit(:user_id, :question_id, :latest_at)
+  end
+
 end
---
+++ app/views/answers/index.json.jbuilder
@@ -0,0 +1,12 @@
+unless @answer.include?(:status)#エラーステータスを持っているかの判定
+  #json.array!で[]を呼び出す
+  json.set! "answers" do
+    json.array!(@answer) do |info|
+      #json.extrat!でデータベース内のデータを表示する
+      json.extract! info, :id, :content
+    end
+  end
+  json.extract! @time, :timestamp
+else#異常ならstatusを返す
+  json.extract! @information, :status
+end
