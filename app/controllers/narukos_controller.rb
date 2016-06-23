6ee816f0ebdeebd72ebec202aaf5ebc9da1f1930
tokikaze0604 <tokikaze0604@gmail.com>
2016-06-23 22:42:38 +0900

    Narukoの不要な記述を削除

--- app/controllers/narukos_controller.rb
+++ app/controllers/narukos_controller.rb
@@ -1,5 +1,4 @@
 class NarukosController < ApplicationController
-  skip_before_filter :verify_authenticity_token
 =begin
   def GCMpush(ids)
     registration_ids = ids#User.find(4).registration_id#create_params[:user_id]
@@ -9,16 +8,12 @@ class NarukosController < ApplicationController
 =end
 
   def create
-=begin
-    user_id = check_user_id(:params[:user_id])
-    @user = User.find(user_id)
-=end
-    # naruko情報の登録
     ids = User.where("id != ?", create_params[:user_id]).pluck(:registration_id)
     logger.debug(ids)
     registration_ids = ids
     options = {data: {message: "naruko"}, collapse_key: "updated_score"}
     response = $gcm.send(registration_ids, options)
+    # GCMとクライアント側両方になげたい
     #GCMpush(@user)
     #@res = { status: "200 OK" }
   end
