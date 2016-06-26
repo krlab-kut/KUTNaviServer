b4b4c1f5899c0c90060d624ed7d163d45ac5ca14
rerejii <hayakawa.shinya.kochi@gmail.com>
2016-06-26 15:51:07 +0900

    最新情報編集フォームの作成 最新情報編集結果の作成 その他諸々修正

--- app/controllers/admin/information_controller.rb
+++ app/controllers/admin/information_controller.rb
@@ -4,7 +4,7 @@ class Admin::InformationController < ApplicationController
   end
 
   def create
-    @information = Information.new(create_params)
+    @information = Information.new(information_params)
     if @information.save
       @save_checker = true
     else
@@ -17,24 +17,33 @@ class Admin::InformationController < ApplicationController
   end
 
   def edit
+    @information = Information.find(id_params[:id])
   end
 
   def show
-    @information = Information.find(show_params[:id])
+    @information = Information.find(id_params[:id])
   end
 
   def update
+    @information = Information.find(id_params[:id])
+    if @information.update(information_params)
+      @update_checker = true
+    else
+      @update_checker = false
+    end
   end
 
   def destroy
+    @information = Information.find(id_params[:id])
+    @information.destroy
   end
 
   private
-  def show_params
+  def id_params
     params.permit(:id)
   end
-  def create_params
-    params.require(:information).permit(:title, :content)
+  def information_params
+    params.require(:information).permit(:title, :content, :place, :start_at)
   end
 
 end
