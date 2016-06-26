2dbb23231ddecbaecc5f02e233d7a5e512fbfb32
ntomoya <ntomoya1010@gmail.com>
2016-06-26 16:17:59 +0000

    研究室一覧のひな型作成

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :user, foreign_key: :place_id
-  has_many :lab, foreign_key: :place_id
+  has_many :labs, foreign_key: :place_id
   has_many :naruko, foreign_key: :place_id
 end
