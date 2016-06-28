0f9ab4ce0ac388f33aadffbc4bf31102d0ee472b
rerejii <hayakawa.shinya.kochi@gmail.com>
2016-06-28 21:59:23 +0900

    一式の作成完了

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,2 +1,11 @@
 class Admin::AnswersController < ApplicationController
+  def destroy
+    @answers = Answer.find(id_params[:id])
+    @answers.destroy
+    redirect_to :back
+  end
+  private
+  def id_params
+    params.permit(:id)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
+  has_many :answers, foreign_key: :question_id, dependent: :destroy
 end
