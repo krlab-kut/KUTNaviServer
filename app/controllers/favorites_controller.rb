644324bf98e91c103715b544d23f5b5e13f68d2e
Kousuke Matsumoto <tokikaze0604@gmail.com>
2016-07-31 00:13:05 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,7 @@ class FavoritesController < ApplicationController
       return
     end
     
-    if Fovirite.exists?(user_id: create_params[:user_id], lab_id: create_params[:place_id])
+    if Fovirite.where("user_id = ? && lab_id = ?" ,create_params[:user_id], create_params[:place_id])exists?
       @res = [status: "200 OK"]
       return
     end
