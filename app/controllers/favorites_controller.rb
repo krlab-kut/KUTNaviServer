61bba824e48750bed0041e56c6b4b49650790b7c
tokikaze0604 <tokikaze0604@gmail.com>
2016-08-03 21:10:41 +0900

    クライアント側コントローラ整形

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,21 @@
 class FavoritesController < ApplicationController
   def create
+    #リクエストに必要なパラメータが含まれているか確認
     unless create_params.has_key?(:user_id) && create_params.has_key?(:place_id)
       @res = {status: "400 Bad_Request"}
       return
     end
-    
+
+    #リクエストのパラメータで指定されたレコードが存在するか確認
+    unless User.exists?(id: index_params[:user_id])
+      @res = {status: "400 Bad_Request"}
+      return
+    end
+    unless Place.exists?(id: index_params[:place_id])
+      @res = {status: "404 Not_found"}
+      return
+    end
+
     if Favorite.where("user_id = ? && lab_id = ?" ,create_params[:user_id], create_params[:place_id]).exists?
       @res = {status: "200 OK"}
       return
@@ -22,8 +33,19 @@ class FavoritesController < ApplicationController
   end
 
   def delete
+    #リクエストに必要なパラメータが含まれているか確認
     unless delete_params.has_key?(:user_id) && delete_params.has_key?(:place_id)
       @res = {status: "400 Bad_Request"}
+      return
+    end
+
+    #リクエストのパラメータで指定されたレコードが存在するか確認
+    unless User.exists?(id: index_params[:user_id])
+      @res = {status: "400 Bad_Request"}
+      return
+    end
+    unless Place.exists?(id: index_params[:place_id])
+      @res = {status: "404 Not_found"}
       return
     end
 
--- app/controllers/narukos_controller.rb
+++ app/controllers/narukos_controller.rb
@@ -1,21 +1,11 @@
 class NarukosController < ApplicationController
-=begin
-  def GCMpush(ids)
-    registration_ids = ids#User.find(4).registration_id#create_params[:user_id]
-    options = {data: {message: "naruko"}, collapse_key: "updated_score"}
-    $gcm.send(registration_ids, options)
-  end
-=end
-
   def create
     ids = User.where("id != ?", create_params[:user_id]).pluck(:registration_id)
     logger.debug(ids)
     registration_ids = ids
     options = {data: {message: "naruko", count: create_params[:count]}, collapse_key: "updated_score"}
-    response = $gcm.send(registration_ids, options)
-    # GCMとクライアント側両方になげたい
-    #GCMpush(@user)
-    #@res = { status: "200 OK" }
+    #稼働期間以外は停止
+    #response = $gcm.send(registration_ids, options)
   end
 
   private
